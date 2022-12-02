7288db2029b9c52485860a9f02fe11ee7c6b7476
mohityede <mohityede755@gmail.com>
2022-12-02 23:09:17 +0530

    back-end for match-schedule-app completed

--- Match-Schedule-app/NodeJS/src/index.js
+++ Match-Schedule-app/NodeJS/src/index.js
@@ -1,4 +1,5 @@
 const app = require("./app");
+// require("./db/defaultDB");
 
 app.listen(8001, () => {
   console.log("App is running on port 8001 " + new Date());
--- Match-Schedule-app/NodeJS/src/routers/index.js
+++ Match-Schedule-app/NodeJS/src/routers/index.js
@@ -8,7 +8,53 @@ const router = new express.Router();
 // You should also implement below filters
 //   * filter to list matches that will be held between given start and end date
 //   * filter for venue
-
+router.get("/fixtures", async (req, res) => {
+  try {
+    const { start_date, end_date, venue } = req.query;
+    const matches = await Fixture.find();
+    if (start_date == undefined && end_date == undefined && venue == undefined) {
+      return res.status(200).send({
+        count: matches.length,
+        records: matches
+      });
+    }
+    let arr = [];
+    matches.forEach(e => {
+      if (start_date && end_date && venue) {
+        const start = new Date(start_date);
+        const end = new Date(end_date);
+        if (e.date >= start && e.date <= end && e.venue == venue) {
+          arr.push(e);
+        }
+      } else if (start_date && end_date) {
+        const start = new Date(start_date);
+        const end = new Date(end_date);
+        if (e.date >= start && e.date <= end) {
+          arr.push(e);
+        }
+      } else if (start_date) {
+        const start = new Date(start_date);
+        if (e.date >= start) {
+          arr.push(e);
+        }
+      } else if (end_date) {
+        const end = new Date(end_date);
+        if (e.date <= end) {
+          arr.push(e);
+        }
+      } else if (venue && venue == e.venue) {
+        arr.push(e);
+      }
+    });
+    const result = {
+      count: arr.length,
+      records: arr
+    }
+    res.status(200).send(result);
+  } catch (err) {
+    res.status(400).send(err);
+  }
+})
 
 // Write a route to create a match fixture i.e., POST /fixtures
 // POST route will take all of these below params
@@ -16,6 +62,15 @@ const router = new express.Router();
 //   * team2
 //   * venue
 //   * date
+
+router.post("/fixtures", async (req, res) => {
+  try {
+    const match = await Fixture(req.body).save();
+    res.status(200).send(match);
+  } catch (err) {
+    res.status(400).send(err);
+  }
+})
 
 
 module.exports = router;
